257399fc4c3abbbcb9648bfbabdf1abaf1b044d1
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-14 17:05:07 -0500

    prompts are working, functions not yet

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 const inquirer = require('inquirer');
-// const prompts = require('./lib/prompts')
+const fs = require('fs');
+const prompts = require('./lib/prompts')
+const generateLogo = require('./lib/shapes');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'Please enter the characters you would like your logo to display (no more than 3).',
-      name: 'characters',
-    },
-    {
-      type: 'input',
-      message: 'What text color would you like (color keyword or hexadecimal number)?',
-      name: 'textColor',
-    },
-    {
-      type: 'list',
-      message: 'What shape would you like?',
-      name: 'shapes',
-      choices: ['Circle', 'Triangle', 'Square'],
-    },
-    {
-      type: 'input',
-      message: 'What color would you like the shape to be (color keyword or hexadecimal number)?',
-      name: 'textColor',
-    },
-  ])
-  .then((response) =>
-    response.confirm === response.password
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
-  );
+//Function to wrap inquirer so that it's not in the global scope
+function init() {
+  inquirer
+    .prompt(prompts)
+    .then((response) =>
+    fs.writeFile('logo.svg', generateLogo(response), (err) =>
+    err ? console.error(err) : console.log('Success!')));
+}
+  // Function call to initialize app
+init();
