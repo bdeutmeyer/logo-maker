06ef275ad7c4a0dbf19aa52b3fe1f973b0d5513a
beth deutmeyer <bethdeutmeyer@gmail.com>
2023-10-15 17:08:44 -0500

    much done, but it's not working yet

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
-const prompts = require('./lib/prompts')
-const generateLogo = require('./lib/shapes');
+const { writeFile } = require('fs/promises');
+const prompts = require('./lib/prompts');
+const { SVG } = require('./lib/classes/svg');
 
 //Function to wrap inquirer so that it's not in the global scope
 function init() {
   inquirer
     .prompt(prompts)
-    .then((response) =>
-    fs.writeFile('logo.svg', generateLogo(response), (err) =>
-    err ? console.error(err) : console.log('Success!')));
+    .then((response) => {
+      const logoSpecs = JSON.parse(response);
+      const newLogo = new SVG(
+        logoSpecs.text,
+        logoSpecs.textColor,
+        logoSpecs.shape,
+        logoSpecs.shapeColor
+      );
+      const chosenLogo = newLogo.render();
+      return writeFile('logo.svg', chosenLogo);
+    })
+    .then(() => {
+      console.log('Logo created successfully!')
+    })
 }
-  // Function call to initialize app
+// Function call to initialize app
 init();
